7a31be77262f51414dc9b666324af69f49cf5453
unknown <nestorortiz017@gmail.com>
2025-07-22 15:05:12 -0500

    backup privileges

--- make_db_backup.sh
+++ make_db_backup.sh
@@ -2,14 +2,15 @@
 
 set -e
 
+#set env variables from .env file
 set -a
 source .env
 set +a
 
 DB_CONTAINER="dj-mariadb"
 DB_NAME="$MYSQL_DATABASE"
-DB_USER="$MYSQL_USER"
-DB_PASS="$MYSQL_PASSWORD"
+DB_USER="root"
+DB_PASS="$MYSQL_ROOT_PASSWORD"
 
 BACKUP_DIR="./backups"
 mkdir -p "$BACKUP_DIR"
@@ -17,7 +18,7 @@ mkdir -p "$BACKUP_DIR"
 TIMESTAMP=$(date +"%Y%m%d-%H%M")
 BACKUP_FILE="$BACKUP_DIR/domjudge-backup-$TIMESTAMP.sql"
 
-echo "[backup-db] Creating backup of database '$DB_NAME' from container '$DB_CONTAINER'..."
+echo "[backup-db] Creating backup of database '$DB_NAME' from container '$DB_CONTAINER' using root..."
 
 docker exec "$DB_CONTAINER" \
   sh -c "exec mysqldump -u$DB_USER -p$DB_PASS $DB_NAME" > "$BACKUP_FILE"
