6673b856006387139382b54929b9361077c9e0c2
unknown <nestorortiz017@gmail.com>
2025-07-22 15:24:19 -0500

    make db backup

--- make_db_backup.sh
+++ make_db_backup.sh
@@ -18,9 +18,9 @@ mkdir -p "$BACKUP_DIR"
 TIMESTAMP=$(date +"%Y%m%d-%H%M")
 BACKUP_FILE="$BACKUP_DIR/domjudge-backup-$TIMESTAMP.sql"
 
-echo "[backup-db] Creating backup of database '$DB_NAME' from container '$DB_CONTAINER' using root..."
+echo "[backup-db] Creating full backup of database '$DB_NAME' from container '$DB_CONTAINER'..."
 
 docker exec "$DB_CONTAINER" \
-  sh -c "exec mysqldump -u$DB_USER -p$DB_PASS $DB_NAME" > "$BACKUP_FILE"
+  sh -c "exec mysqldump --add-drop-database --databases $DB_NAME -u$DB_USER -p$DB_PASS" > "$BACKUP_FILE"
 
 echo "[backup-db] Backup saved to: $BACKUP_FILE"
